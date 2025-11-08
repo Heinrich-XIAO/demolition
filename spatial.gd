e628cae71d4bb8529df242b93c6544282bd48068
Heinrich-XIAO <74563446+Heinrich-XIAO@users.noreply.github.com>
2025-11-08 15:02:38 -0600

    fix collision

--- spatial.gd
+++ spatial.gd
@@ -1,21 +1,35 @@
 extends Node3D
 
+
 var block_scene = preload("res://block.tscn")
 
 func new_block(position: Vector3, is_fallen: bool, length: float, color: Color):
 	var block = block_scene.instantiate()
+	
+	# Adjust collision shape
+	block.get_children()[0].shape = BoxShape3D.new()
 	block.get_children()[0].shape.size.x = length
-	block.get_children()[1].mesh.size.x = length
-	block.get_children()[1].mesh.material.albedo_color = color
+	
+	# Adjust mesh size and color
+	block.get_children()[1].scale.x = length  # scale instead of mesh.size
+	var mat = block.get_children()[1].get_active_material(0)
+	if mat:
+		mat.albedo_color = color
+	else:
+		mat = StandardMaterial3D.new()
+		mat.albedo_color = color
+		block.get_children()[1].set_surface_override_material(0, mat)
 	
 	if not is_fallen:
-		block.rotation.z = PI/2
-		block.position.y += length/2
+		block.rotation.z = PI / 2
+		block.position.y += length / 2
+	
 	block.position += position
 	return block
 
 func _ready() -> void:
-	add_child(new_block(Vector3(10, 0, 0), false, 10.0, Color(1, 0.1, 1, 1)))
+	add_child(new_block(Vector3(10, 0, 0), false, 10.0, Color(1, 0.1, 0, 1)))
+	add_child(new_block(Vector3(0, 0, 0), false, 5.0, Color(0.1, 0.9, 0.3, 1)))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
