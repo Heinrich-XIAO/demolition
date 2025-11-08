784da5c7adc5b926ed38765e999ef089aba5176f
Heinrich-XIAO <74563446+Heinrich-XIAO@users.noreply.github.com>
2025-11-08 11:19:24 -0600

    amke things look better

--- spatial.gd
+++ spatial.gd
@@ -2,12 +2,20 @@ extends Node3D
 
 var block_scene = preload("res://block.tscn")
 
-# Called when the node enters the scene tree for the first time.
+func new_block(position: Vector3, is_fallen: bool, length: float, color: Color):
+	var block = block_scene.instantiate()
+	block.get_children()[0].shape.size.x = length
+	block.get_children()[1].mesh.size.x = length
+	block.get_children()[1].mesh.material.albedo_color = color
+	
+	if not is_fallen:
+		block.rotation.z = PI/2
+		block.position.y += length/2
+	block.position += position
+	return block
+
 func _ready() -> void:
-	var instance = block_scene.instantiate()
-	instance.get_children()[0].shape.size.x = 10.0
-	instance.get_children()[1].mesh.size.x = 10.0
-	add_child(instance)
+	add_child(new_block(Vector3(10, 0, 0), false, 10.0, Color(1, 0.1, 1, 1)))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
