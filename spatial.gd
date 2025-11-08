892d479685e4977cb3c1b4e14a290f24c3bbf7c9
Heinrich-XIAO <74563446+Heinrich-XIAO@users.noreply.github.com>
2025-11-08 16:00:25 -0600

    create starting scene

--- spatial.gd
+++ spatial.gd
@@ -3,7 +3,7 @@ extends Node3D
 
 var block_scene = preload("res://block.tscn")
 
-func new_block(position: Vector3, is_fallen: bool, length: float, color: Color):
+func new_block(position: Vector3, is_fallen: bool, length: float, color: Color, rotation: Vector3):
 	var block = block_scene.instantiate()
 	
 	# Adjust collision shape
@@ -12,24 +12,21 @@ func new_block(position: Vector3, is_fallen: bool, length: float, color: Color):
 	
 	# Adjust mesh size and color
 	block.get_children()[1].scale.x = length  # scale instead of mesh.size
-	var mat = block.get_children()[1].get_active_material(0)
-	if mat:
-		mat.albedo_color = color
-	else:
-		mat = StandardMaterial3D.new()
-		mat.albedo_color = color
-		block.get_children()[1].set_surface_override_material(0, mat)
+	var mat = block.get_children()[1].get_active_material(0).duplicate()
+	mat.albedo_color = color
+	block.get_children()[1].set_surface_override_material(0, mat)
 	
 	if not is_fallen:
 		block.rotation.z = PI / 2
 		block.position.y += length / 2
 	
 	block.position += position
+	block.rotation += rotation
 	return block
 
 func _ready() -> void:
-	add_child(new_block(Vector3(10, 0, 0), false, 10.0, Color(1, 0.1, 0, 1)))
-	add_child(new_block(Vector3(0, 0, 0), false, 5.0, Color(0.1, 0.9, 0.3, 1)))
+	add_child(new_block(Vector3(10, 0, 0), false, 10.0, Color(1, 0.4, 0.1, 1.0), Vector3.ZERO))
+	add_child(new_block(Vector3(0, 0, 0), true, 5.0, Color(0.1, 0.9, 0.3, 1), Vector3(0, PI/2, 0)))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
